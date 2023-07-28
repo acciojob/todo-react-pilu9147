61991572825e86f2cfdbb4946055d6cbff342330
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-07-28 06:20:18 +0000

    vvvvvvvvvvvvv

---
+++ src/components/TodoList.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    setTodos((prevTodos) =>
+      prevTodos.filter((_, todoIndex) => todoIndex !== index)
+    );
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={handleInputChange}
+        placeholder="Add a new todo"
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
